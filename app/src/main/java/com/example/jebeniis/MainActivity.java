84b13be54d1db2432c6419b57c352c8b3745bcb8
M. David <david.majdandzic@inovatrend.com>
2022-07-04 10:40:47 +0200

    Implement question parsing

--- app/src/main/java/com/example/jebeniis/MainActivity.java
+++ app/src/main/java/com/example/jebeniis/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.jebeniis;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public final class MainActivity extends AppCompatActivity {
     @Override
@@ -14,13 +21,34 @@ public final class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println(loadJSONFromAsset());
+        HashMap<String, String> questions = getQuestions();
+        System.out.println(questions);
     }
 
-    public String loadJSONFromAsset() {
+    public HashMap<String, String> getQuestions() {
+        String content = loadAsset("is.json");
+        HashMap<String, String> questions = new HashMap<>();
+        try {
+            JSONObject jsonObject = new JSONObject(content);
+            jsonObject.keys().forEachRemaining(key -> {
+                try {
+                    String value = jsonObject.getString(key);
+                    questions.put(key, value);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            });
+            return questions;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String loadAsset(String asset) {
         String json = null;
         try {
-            InputStream is = this.getAssets().open("is.json");
+            InputStream is = this.getAssets().open(asset);
             int size = is.available();
             byte[] buffer = new byte[size];
             is.read(buffer);
